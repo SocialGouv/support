a4f9feb22c5c444b140e6b5a7e95c04c5f088463
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-02-10 11:28:50 +0100

    fix: add liens presentations

--- sidebars.js
+++ sidebars.js
@@ -15,37 +15,75 @@
 const sidebars = {
   tutorialSidebar: [
     {
-      type: 'category',
-      label: 'Démarrage',
-      items: ['init/presentation', 'init/fonctionnement', 'init/accueil-produits'],
-      collapsed: false,
-      collapsible: true
-    },
-    {
-      type: 'category',
-      label: 'Standards',
-      items: ['standards/developpement', 'standards/databases', 'standards/securite', 'standards/kubernetes', 'standards/tests', 'standards/mobile', 'standards/docker'],
-    },
-    {
-      type: 'category',
-      label: 'Infrastructure',
+      type: "category",
+      label: "Démarrage",
       items: [
-        'infrastructure/presentation', 
-        {
-          type: 'doc',
-          id: 'infrastructure/pra',
-          label: 'PRA/PCA'
-        }, 
-        'infrastructure/disaster-recovery', 
-        'infrastructure/emails'
+        "init/presentation",
+        "init/fonctionnement",
+        "init/accueil-produits",
+      ],
+      collapsed: false,
+      collapsible: true,
+    },
+    {
+      type: "category",
+      label: "Standards",
+      items: [
+        "standards/developpement",
+        "standards/databases",
+        "standards/securite",
+        "standards/kubernetes",
+        "standards/tests",
+        "standards/mobile",
+        "standards/docker",
       ],
     },
-    'faq',
     {
-      type: 'category',
-      label: 'Workshops',
-      items: ['workshops/production', 'workshops/kubernetes'],
-    }
+      type: "category",
+      label: "Infrastructure",
+      items: [
+        "infrastructure/presentation",
+        {
+          type: "doc",
+          id: "infrastructure/pra",
+          label: "PRA/PCA",
+        },
+        "infrastructure/disaster-recovery",
+        "infrastructure/emails",
+      ],
+    },
+    "faq",
+    {
+      type: "category",
+      label: "Workshops",
+      items: ["workshops/production", "workshops/kubernetes"],
+    },
+    {
+      type: "category",
+      label: "Présentations",
+      items: [
+        {
+          type: "link",
+          label: "Guide de survie de la PR",
+          href: "https://docs.google.com/presentation/d/1x9H2-SHsF6mXuByijX7u3gTC5CeT4nmbpI1z-NOtyUE",
+        },
+        {
+          type: "link",
+          label: "Template",
+          href: "https://docs.google.com/presentation/d/1DQMVxy87BEQ-VDF1tJ03jpPBbQnZtTfcDucfoMwVSM0",
+        },
+        {
+          type: "link",
+          label: "Kontinuous",
+          href: "https://docs.google.com/presentation/d/18TUqf4ew2lNp8w02mY7YiaKyWaEVneUA1ME4VzDlhsM",
+        },
+        {
+          type: "link",
+          label: "Metabase",
+          href: "https://docs.google.com/presentation/d/1ov0sU89iiVtebNPplolaNwYU8JWIaXc1M6Z7mfn1nKA",
+        },
+      ],
+    },
   ],
 };
 
